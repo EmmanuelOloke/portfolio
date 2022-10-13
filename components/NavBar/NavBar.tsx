c9fbf523cf66fb9a6df30fd5b872650eb72818a4
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-13 02:17:13 +0100

    imported and tested font awesome icon is working

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+
 const NavBar = () => {
-  return <div>This will be the NavBar</div>;
+  return (
+    <>
+      <div>
+        <FontAwesomeIcon icon={faCode} />
+        <div>This will be the NavBar</div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
