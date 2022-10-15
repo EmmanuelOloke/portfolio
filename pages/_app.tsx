98b4c1ca116e1363210b14cf1cab2c927934e3ca
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-15 02:21:19 +0100

    installed chakra ui and wrapped app with it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
