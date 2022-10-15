09c1713dc6a89c1361ed5f1b3b213ec76183ada2
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-15 04:09:14 +0100

    initial navbar design done

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { Icon, Flex, Box, Link, IconButton, useColorMode } from '@chakra-ui/react';
+import { FaCode, FaSun, FaMoon } from 'react-icons/fa';
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <>
-      <div>
-        <FontAwesomeIcon icon={faCode} />
-        <div>This will be the NavBar</div>
-      </div>
-    </>
+    <Flex justifyContent="center" boxShadow="md">
+      <Flex
+        justifyContent="space-between"
+        py={5}
+        alignItems="center"
+        width="90vw"
+        w={{ base: '87vw', sm: '80vw', md: '80vw', lg: '88vw', xl: '87vw', '2xl': '65vw' }}
+      >
+        <Box>
+          <Icon as={FaCode} fontSize="3xl" />
+        </Box>
+
+        <Flex gap={14} fontSize="md" textTransform="uppercase" alignItems="center">
+          <Link>About</Link>
+          <Link>Projects</Link>
+          <Link>Contacts</Link>
+          <IconButton
+            aria-label="icon"
+            icon={colorMode === 'light' ? <FaMoon /> : <FaSun />}
+            onClick={toggleColorMode}
+          />
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
