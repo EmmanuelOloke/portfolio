756c7e82afae5b16237d1cb3186aaad633d590bd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-18 22:46:04 +0100

    imported and rendered Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,15 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 import NavBar from '../components/NavBar/NavBar';
+import Hero from '../components/Hero/Hero';
 
 const Home: NextPage = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
