d8a4f06816320f8e736c7692fb6135c892d92e86
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-19 14:42:39 +0100

    Hero section design

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,13 +1,81 @@
 import react from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Flex, Link, Button, Image } from '@chakra-ui/react';
 
 const Hero = () => {
   return (
-    <Box>
-      <Text fontSize="3xl">
-        Hey there! <span className="wave">👋🏾</span>
-      </Text>
-    </Box>
+    <Flex
+      flexDirection={{
+        base: 'column',
+        sm: 'column',
+        md: 'column',
+        lg: 'row',
+        xl: 'row',
+        '2xl': 'row',
+      }}
+      alignItems={{
+        base: 'center',
+        sm: 'center',
+        md: 'center',
+        lg: 'center',
+        xl: 'center',
+        '2xl': 'center',
+      }}
+      justifyContent={{
+        base: 'center',
+        sm: 'center',
+        md: 'center',
+        lg: 'center',
+        xl: 'center',
+        '2xl': 'center',
+      }}
+      mt={{ base: '8rem', sm: '8rem', md: '8rem', lg: 40, xl: 40, '2xl': 40 }}
+      gap={{ base: 20, sm: 20, md: 20, lg: 60, xl: 60, '2xl': 60 }}
+    >
+      <Box>
+        <Text fontSize={{ base: '2xl', sm: '2xl', md: '2xl', lg: 'lg', xl: '3xl', '2xl': '3xl' }}>
+          Hey there <span className="wave">👋🏾</span> I am
+        </Text>
+        <Text
+          fontSize={{ base: '4xl', sm: '4xl', md: '4xl', lg: '5xl', xl: '5xl', '2xl': '5xl' }}
+          fontWeight="bold"
+        >
+          Emmanuel Oloke
+        </Text>
+        <Text
+          mt={2}
+          fontSize={{ base: '3xl', sm: '3xl', md: '3xl', lg: '5xl', xl: '5xl', '2xl': '5xl' }}
+          fontWeight="bold"
+          opacity="0.5"
+        >
+          I build beautiful software
+        </Text>
+        <Text
+          mt={5}
+          w={{ base: '20rem', sm: '25rem', md: '35rem', lg: '35rem', xl: '35rem', '2xl': '35rem' }}
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique molestiae cupiditate
+          voluptatum corrupti possimus explicabo culpa, ab sed natus maiores. Vel iure similique
+          unde illum officia architecto suscipit! Qui, nisi?
+        </Text>
+        <Box mt={10}>
+          <Link
+            className="resume-btn"
+            href="/resume.pdf"
+            target="_blank"
+            outline="solid"
+            px={14}
+            py={4}
+            borderRadius={3}
+          >
+            <Button variant="none">Resume</Button>
+          </Link>
+        </Box>
+      </Box>
+
+      <Box className="gradient-border" w={{ base: '20rem', sm: '20rem', md: '20rem' }}>
+        <Image src="memoji.png" alt="Emmanuel Oloke's Memoji" />
+      </Box>
+    </Flex>
   );
 };
 
