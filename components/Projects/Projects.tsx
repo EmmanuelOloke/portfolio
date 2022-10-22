a25cb53ef90032d6d568c84207d5a007a2a69ebb
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-22 11:01:38 +0100

    created the projects component

---
+++ components/Projects/Projects.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, Box, Flex } from '@chakra-ui/react';
+
+const Projects = () => {
+  return <Flex mb={'5rem'}>Here you will find the projects I have worked on</Flex>;
+};
+
+export default Projects;
