1961832010bc2a28bd4cbe215588399097b31df2
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-24 13:21:58 +0100

    changed tools icon sizes and removed opacity from title text to add color

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -45,7 +45,7 @@ const About = () => {
         fontSize={'1.2rem'}
         w={{ base: '25rem', sm: '25rem', md: '25rem', lg: '30rem', xl: '40rem', '2xl': '40rem' }}
       >
-        <Text fontSize="2.5rem" textAlign="center" fontWeight="bold" opacity="0.7">
+        <Text fontSize="2.5rem" textAlign="center" fontWeight="bold" color="#8c9095">
           About Me
         </Text>
         <Divider mt={'1rem'} mb={'1rem'} />
@@ -82,50 +82,50 @@ const About = () => {
         </Text>
         <Flex gap={'15px'} alignItems="center" flexWrap={'wrap'}>
           <Box className="tools">
-            <Image src="/html-logo.png" alt="html 5 logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/html-logo.png" alt="html 5 logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="sm">
               HTML 5
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/css-logo.webp" alt="css3 logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/css-logo.webp" alt="css3 logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               CSS 3
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/js-logo.png" alt="javascript logo" borderRadius={'10px'} w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/js-logo.png" alt="javascript logo" borderRadius={'10px'} w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               JavaScript
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/typescript-logo.png" alt="typescript logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/typescript-logo.png" alt="typescript logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               TypeScript
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/node-logo.webp" alt="node logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/node-logo.webp" alt="node logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               NodeJS
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/nextjs.png" alt="next logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/nextjs.png" alt="next logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               NextJS
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/react.webp" alt="react logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/react.webp" alt="react logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               ReactJS
             </Text>
           </Box>
           <Box className="tools">
-            <Image src="/mongo.webp" alt="mongodb logo" w={'4rem'} />
-            <Text opacity="0.7" fontWeight="bold">
+            <Image src="/mongo.webp" alt="mongodb logo" w={'3rem'} />
+            <Text opacity="0.7" fontWeight="bold" fontSize="md">
               MongoDB
             </Text>
           </Box>
