ab6e2bb8a1eb15633a8a7a54c0200550ec7e3b16
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-25 23:58:34 +0100

    imported and initialised AOS, used next/head for site details, imported AOS css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import AOS from 'aos';
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
+
+import 'aos/dist/aos.css';
 
 import '../components/Hero/style.css';
 import '../components/About/style.css';
@@ -13,10 +18,24 @@ import '../components/Projects/style.css';
 import '../components/NavBar/style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Emmanuel Oloke</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="description" content="Software Engineer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
