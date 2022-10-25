a4fc1060307593e406d9ae2fefecedc542a8da35
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-10-25 17:53:00 +0100

    imported project styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ config.autoAddCss = false;
 import '../components/Hero/style.css';
 import '../components/About/style.css';
 import '../components/Contact/style.css';
+import '../components/Projects/style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
