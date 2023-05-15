5b7e3b227448cb64e2a643becf07ea0d1e27e1f9
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-15 17:19:36 +0100

    created a data file for known tech tools

---
+++ public/data/techAndTools.ts
@@ -0,0 +1,67 @@
+import { TechTools } from '../types/techtools';
+
+export const techAndTools: TechTools[] = [
+  {
+    id: 1,
+    src: '/tech-images/html.png',
+    alt: 'html 5 logo',
+    textColor: '#8c9095',
+    toolName: 'HTML 5',
+  },
+  {
+    id: 2,
+    src: '/tech-images/css.webp',
+    alt: 'css 3 logo',
+    textColor: '#8c9095',
+    toolName: 'CSS 3',
+  },
+  {
+    id: 3,
+    src: '/tech-images/js.png',
+    alt: 'javascript logo',
+    textColor: '#8c9095',
+    toolName: 'JavaScript',
+  },
+  {
+    id: 4,
+    src: '/tech-images/typescript.png',
+    alt: 'typescript logo',
+    textColor: '#8c9095',
+    toolName: 'TypeScript',
+  },
+  {
+    id: 5,
+    src: '/tech-images/node.webp',
+    alt: 'node logo',
+    textColor: '#8c9095',
+    toolName: 'NodeJS',
+  },
+  {
+    id: 6,
+    src: '/tech-images/nextjs.png',
+    alt: 'next logo',
+    textColor: '#8c9095',
+    toolName: 'NextJS',
+  },
+  {
+    id: 7,
+    src: '/tech-images/react.webp',
+    alt: 'react logo',
+    textColor: '#8c9095',
+    toolName: 'ReactJS',
+  },
+  {
+    id: 8,
+    src: '/tech-images/vue.png',
+    alt: 'vue logo',
+    textColor: '#8c9095',
+    toolName: 'VueJS',
+  },
+  {
+    id: 9,
+    src: '/tech-images/mongo.webp',
+    alt: 'mongodb logo',
+    textColor: '#8c9095',
+    toolName: 'MongoDB',
+  },
+];
