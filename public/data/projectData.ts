cd5b498407e35f98b3e244d40cdc0f698a6bdd55
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-16 23:38:50 +0100

    created data file for projects

---
+++ public/data/projectData.ts
@@ -0,0 +1,13 @@
+import { Project } from '../types/projects';
+
+export const projectData: Project[] = [
+  {
+    id: 1,
+    previewImgSrc: 'string',
+    previewImgAlt: 'string',
+    projectTitleTextColor: '#8c9095',
+    projectDescription: 'string',
+    githubRepoLink: 'string',
+    liveLink: 'string',
+  },
+];
