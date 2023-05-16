74469579b8fb195ccafd7238e6eaeda1d5bbcaaf
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-16 23:40:17 +0100

    created Project type

---
+++ public/types/projects.ts
@@ -0,0 +1,9 @@
+export type Project = {
+  id: number;
+  previewImgSrc: string;
+  previewImgAlt: string;
+  projectTitleTextColor: string;
+  projectDescription: string;
+  githubRepoLink: string;
+  liveLink: string;
+};
